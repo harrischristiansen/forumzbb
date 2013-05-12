dd314a17ee49b87bdfaa8c69e5b9a7f328389c40
Harris Christiansen <harrischristiansen@mac.com>
2013-05-12 16:40:31 -0600

    Fixed devTools file

--- Functions/forumz.devTools.php
+++ Functions/forumz.devTools.php
@@ -1,7 +1,7 @@
 <?php
 // Harris Christiansen
 // Created 10-26-12
-// Updated 10-30-12
+// Updated 5-12-13
 
 function writeSessionData() {
 	global $userData;
@@ -10,9 +10,9 @@ function writeSessionData() {
 	$outputText = $outputText."Account ID: ".$userData['actID']."<br>";
 	$outputText = $outputText."Logged In Status: ".$userData['loggedIn']."<br>";
 	$outputText = $outputText."Rank ID: ".$userData['rankID']."<br>";
+	$outputText = $outputText."Email: ".$userData['email']."<br>";
 	$outputText = $outputText."Edit Site Settings: ".$userData['permissions']['editSiteSettings']."<br>";
 	$outputText = $outputText."Edit Members Rank: ".$userData['permissions']['editMemberRank']."<br>";
 	addImportantNotice($outputText);
-	display('viewError');
 }
 ?>
