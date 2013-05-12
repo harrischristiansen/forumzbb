ad5cb868cd67d7375f2b0c1dfce8b13390654577
Harris Christiansen <harrischristiansen@mac.com>
2013-05-12 15:25:47 -0600

    Renamed from forumz.viewBlog.php

---
+++ Functions/forumz.blog.php
@@ -0,0 +1,119 @@
+<?php
+// Harris Christiansen
+// Created 11-02-12
+// Updated 5-12-12
+
+// Blog View System
+
+
+function getBlogEntry($entryID) {
+	global $con, $sqlQueries;
+	$sql = "SELECT * FROM blogs WHERE ID='$entryID'";
+	$result = mysqli_query($con, $sql) or die ("Query failed: getBlogEntry");
+	$sqlQueries++;
+	$resultArray = mysqli_fetch_array($result);
+	return $resultArray;
+}
+
+function getBlogEntries($startID, $endID) {
+	global $con, $sqlQueries;
+	$sql = "SELECT * FROM blogs WHERE ID>='$startID' AND ID<'$endID' ORDER BY ID DESC";
+	$result = mysqli_query($con, $sql) or die ("Query failed: getBlogEntries");
+	$sqlQueries++;
+	return $result;
+}
+
+function viewBlogEntries() {
+	global $siteSettings, $pageID;
+	$blogEntriesPerPage=$siteSettings['blogEntriesPerPage'];
+	$startID=getNumBlogEntries()-$blogEntriesPerPage;
+	$endID=getNumBlogEntries();
+	if($pageID!="none"&&$pageID>=2) {
+		$numToSubtract=($pageID-1)*$blogEntriesPerPage;
+		$startID-=$numToSubtract;
+		$endID-=$numToSubtract;
+	}
+	
+	
+	$blogEntries=getBlogEntries($startID,$endID);
+	
+	while($entry = mysqli_fetch_array($blogEntries)) {
+		$blogLink=$siteSettings['siteURLShort']."blog/".$entry['ID'];
+		displayHomePageBlogEntry(getMemberName($entry['Author']),$entry['AuthorDate'],$entry['Title'],$entry['Post'],$blogLink);
+	}
+	if(mysqli_num_rows($blogEntries)==0) {
+		viewFailure("No Entries Were Found On This Page");
+	}
+}
+
+function viewBlogPageBlogEntry() {
+	global $pageID;
+	$blogEntry = getBlogEntry($pageID);
+	displayBlogEntry(getMemberName($blogEntry['Author']),$blogEntry['AuthorDate'],$blogEntry['AuthorTime'],$blogEntry['Title'],$blogEntry['Post']);
+}
+
+
+function getNumBlogEntries() {
+	global $con, $sqlQueries;
+	$sql = "SELECT * FROM blogs";
+	$result = mysqli_query($con, $sql) or die ("Query failed: getNumBlogEntries");
+	$sqlQueries++;
+	return mysqli_num_rows($result);
+}
+
+function isFirstPage() {
+	global $pageID;
+	if($pageID=="none"||$pageID==0||$pageID==1) {
+		return true;
+	} else { return false; }
+}
+
+function isLastPage() {
+	global $pageID,$siteSettings;
+	$lastPage = ceil(getNumBlogEntries()/$siteSettings['blogEntriesPerPage']);
+	if($pageID>=$lastPage) {
+		return true;
+	} else { return false; }
+}
+
+function getNextPageLink() {
+	global $pageID,$siteSettings;
+	if($pageID<=1) {
+		$pageID=1;
+	}
+	return $siteSettings['siteURLShort']."home/".($pageID+1);
+}
+
+function getPreviousPageLink() {
+	global $pageID,$siteSettings;
+	$lastPage = ceil(getNumBlogEntries()/$siteSettings['blogEntriesPerPage']);
+	if($pageID>$lastPage) {
+		$pageID=$lastPage+1;
+	}
+	return $siteSettings['siteURLShort']."home/".($pageID-1);
+}
+
+
+// Blog Comment View System
+
+function getBlogComments($blogID) {
+	global $con, $sqlQueries;
+	$sql = "SELECT * FROM blogComments WHERE blogID='$blogID' ORDER BY idNum";
+	$result = mysqli_query($con, $sql) or die ("Query failed: getBlogComments");
+	$sqlQueries++;
+	return $result;
+}
+
+function viewBlogComments() {
+	global$pageID;
+	$blogComments=getBlogComments($pageID);
+
+	while($comment = mysqli_fetch_array($blogComments)) {
+		displayBlogComment(getMemberName($comment['posterID']),$comment['date'],$comment['time'],$comment['comment']);
+	}
+}
+
+// Blog Comment Post System
+
+
+?>
