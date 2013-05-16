e9a039d16ffc9d811ebd20227b5cd6ff49740823
Harris Christiansen <harrischristiansen@mac.com>
2013-05-15 23:13:54 -0600

    Added Additional Replacement to dbQuery

--- Functions/forumz.databaseTools.php
+++ Functions/forumz.databaseTools.php
@@ -10,9 +10,11 @@
 		$tableTitle=session_name();
 		$tableTitle_from="FROM ".$tableTitle."_";
 		$tableTitle_upd="UPDATE ".$tableTitle."_";
+		$tableTitle_inst="INSERT INTO ".$tableTitle."_";
 		// Append Table Title
 		$sql = str_replace("FROM ",$tableTitle_from,$sql);
 		$sql = str_replace("UPDATE ",$tableTitle_upd,$sql);
+		$sql = str_replace("INSERT INTO ",$tableTitle_inst,$sql);
 		// Execute Query and Return Result
 		return mysqli_query($con, $sql);
 	}
