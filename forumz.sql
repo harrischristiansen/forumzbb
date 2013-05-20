eac637beefab51e10f6dd141109e5f57a7037797
Harris Christiansen <harrischristiansen@mac.com>
2013-05-19 20:43:50 -0600

    Updated Forumz SQL

--- forumz.sql
+++ forumz.sql
@@ -1,70 +1,95 @@
 SET NAMES latin1;
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `accounts` (
-  `actID` int(11) NOT NULL,
+CREATE TABLE `forumzDev_accounts` (
+  `actID` text NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `email` text NOT NULL,
   `actStatus` int(11) NOT NULL COMMENT '0=active, 1=waitingEmailVerification, 2=waitingAdminVerification, 3=banned',
   `rankID` int(11) NOT NULL,
   `joinDate` text NOT NULL,
-  `ipAddress` text NOT NULL
+  `joinIP` text NOT NULL,
+  `lastLogin` text NOT NULL,
+  `lastLoginIP` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `accounts` values('0','Harris','7af4896825dfc7e94f8a1d6846a5a2d4','cloudy243@me.com','0','2','10-03-12','205.124.117.24'),
- ('1','TestUser','7a95dec218ffaaf8992bb48b4bd94367','testUser@forumzbb.com','0','1','05-12-13','67.161.245.43');
+insert into `forumzDev_accounts` values('0','Harris','7af4896825dfc7e94f8a1d6846a5a2d4','cloudy243@me.com','0','2','10-03-12','205.124.117.24','5-19-13','67.161.245.43'),
+ ('1','TestUser','7a95dec218ffaaf8992bb48b4bd94367','testUser@forumzbb.com','0','1','05-12-13','67.161.245.43','5-19-13','67.161.245.43'),
+ ('Anonymous','Anonymous','-','Anonymous','0','0','-','-','-','-');
 
-CREATE TABLE `blogComments` (
+CREATE TABLE `forumzDev_blogComments` (
   `idNum` int(11) NOT NULL,
   `blogID` int(11) NOT NULL,
-  `posterID` int(11) NOT NULL,
+  `posterID` text NOT NULL,
   `date` text NOT NULL,
   `time` text NOT NULL,
   `comment` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `blogComments` values('0','11','0','05-12-13','19:35','Test of Comment System');
 
-CREATE TABLE `blogs` (
+CREATE TABLE `forumzDev_blogs` (
   `ID` int(11) NOT NULL,
   `Title` text NOT NULL,
   `Author` text NOT NULL,
   `AuthorDate` text NOT NULL,
   `AuthorTime` text NOT NULL,
-  `Post` text NOT NULL,
-  `numComments` int(11) NOT NULL
+  `Post` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `blogs` values('1','Entry 2','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('2','Entry 3','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('3','Entry 4','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('4','Entry 5','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('5','Entry 6','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('6','Entry 7','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('7','Entry 8','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('8','Entry 9','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('9','Entry 10','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('10','Entry 11','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('11','Entry 12','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2'),
- ('0','Entry 1','0','11-03-12','13:19:50','Test Of Blog System<br>\nBlah Blah Blah<br>\nHum Hum Hum','2');
+insert into `forumzDev_blogs` values('0','Forumz 0.5.2 Dev','0','5-17-13','23:03','Development Site For ForumzBB - Version 0.5.2');
 
-CREATE TABLE `ranks` (
+CREATE TABLE `forumzDev_pages` (
+  `pageURL` text NOT NULL,
+  `idDependant` text NOT NULL,
+  `displayFile` text NOT NULL,
+  `falseDisplayFile` text NOT NULL,
+  `functionCall` text NOT NULL,
+  `pageName` text NOT NULL,
+  `breadcrumbTitle` text NOT NULL,
+  `trueMsg` text NOT NULL,
+  `falseMsg` text NOT NULL,
+  `requireLogin` text NOT NULL,
+  `siteRequireLoginApplies` text NOT NULL,
+  `requirePermission` text NOT NULL,
+  `requireFormSubmitted` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+insert into `forumzDev_pages` values('home','','viewHome','','','Home','','','','','true','',''),
+ ('login','','viewHome','','loginUser','Login','Login','You Are Logged In','Please Login Above','','','','loginSubmitted'),
+ ('blog','','viewBlog','','checkBlogEntryExists','Blog Post','Blog Post','','','','true','',''),
+ ('register','','','viewRegistration','registerUser','Register','Register','','','','','','registerSubmitted'),
+ ('logout','','viewHome','','logoutUser','Logout','Logout','','','','','',''),
+ ('membersList','','viewMembersList','','','Members List','Members','','','','','',''),
+ ('membersList','changeUserRank','','','setUserRank','','Set Member Rank','','Action Denied','true','','editMemberRank','newRank'),
+ ('devOutput','','','','writeSessionData','Dev Output','Dev Output','','','','','',''),
+ ('blog','reply','viewBlog','viewBlog','addBlogComment','','Post Comment','','','','true','postBlogComments','commentSubmitted'),
+ ('controlPanel','','viewControlPanel','','','Control Panel','Control Panel','Select An Item On The Left','','true','','',''),
+ ('controlPanel','changePassword','viewControlPanel','viewControlPanel','updateAccountPassword','Control Panel Change Password','Control Panel Change Password','','','true','','','cpFormSubmitted'),
+ ('controlPanel','editProfile','viewControlPanel','viewControlPanel','updateAccountProfile','Control Panel Update Profile','Control Panel Update Profile','','','true','','','cpFormSubmitted'),
+ ('controlPanel','changePreferences','viewControlPanel','viewControlPanel','','Control Panel Update Preferences','Control Panel Update Preferences','','','true','','','cpFormSubmitted'),
+ ('controlPanel','editSiteSettings','viewControlPanel','viewControlPanel','updateSiteSettings','Control Panel Update Site Settings','Control Panel Update Site Settings','','','true','','editSiteSettings','cpFormSubmitted'),
+ ('controlPanel','addRank','viewControlPanel','viewControlPanel','addSiteRank','Control Panel Add Rank','Control Panel Add Rank','','','true','','editRanks','cpFormSubmitted'),
+ ('controlPanel','editRanks','viewControlPanel','viewControlPanel','updateRank','Control Panel Edit Rank','Control Panel Edit Rank','','','true','','editRanks','cpFormSubmitted'),
+ ('composeEntry','','viewBlog','viewBlogCompose','addBlogEntry','Compose Blog Entry','New Entry','','','true','','postBlogEntries','blogComposeSubmitted');
+
+CREATE TABLE `forumzDev_ranks` (
   `rankID` int(11) NOT NULL,
   `rankOrder` int(11) NOT NULL,
   `rankName` text NOT NULL,
   `editSiteSettings` text NOT NULL,
   `editMemberRank` text NOT NULL,
+  `editRanks` text NOT NULL,
   `postBlogEntries` text NOT NULL,
   `postBlogComments` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `ranks` values('0','0','Anonymous','false','false','false','false'),
- ('1','1','Member','false','false','false','true'),
- ('2','3','Admin','true','true','true','true'),
- ('3','2','Level 2 Member','false','false','false','true');
+insert into `forumzDev_ranks` values('0','0','Anonymous','false','false','false','false','false'),
+ ('1','1','Member','','','','','true'),
+ ('2','1000','Admin','true','true','true','true','true'),
+ ('3','3','Level 2 Member','false','false','false','false','true');
 
-CREATE TABLE `siteSettings` (
+CREATE TABLE `forumzDev_siteSettings` (
   `settingsProfile` text NOT NULL,
   `siteName` text NOT NULL,
   `siteMotd` text NOT NULL,
@@ -78,6 +103,6 @@ CREATE TABLE `siteSettings` (
   `blogEntriesPerPage` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `siteSettings` values('1','Forumz','','Dev Version 0.5.2 © 2012 Forumzbb','SkyBlue','','','false','false','false','4');
+insert into `forumzDev_siteSettings` values('1','Forumz','','Dev Version 0.5.2 © 2013 Forumzbb','SkyBlue','','','false','false','false','4');
 
 SET FOREIGN_KEY_CHECKS = 1;
